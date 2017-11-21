251831911a6ec64356deae5a2a11b7f9089e1972
TheBITLINK aka BIT <me@thebitlink.com>
2017-11-21 18:26:43 -0400

    FreeBSD workaround

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const path = require('path')
 
 module.exports = {
   download: require('./downloader'),
-  path: path.join(__dirname, bin),
-  probePath: path.join(__dirname, probeBin)
+  path: platform === 'freebsd' ? '/usr/local/bin/ffmpeg' : path.join(__dirname, bin),
+  probePath: platform === 'freebsd' ? '/usr/local/bin/ffprobe' : path.join(__dirname, probeBin)
 }
-
